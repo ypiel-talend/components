af13529d66b3d68eb14aeaf8351495a5ad938bdf
Ivan Honchar <ivan.honchar@globallogic.com>
2017-08-04 15:15:07 +0300

    feat(TCOMP-613): add TGoogleFusionTableInputSource class

--- components/components-google/components-google-runtime/src/main/java/org/talend/components/google/runtime/reader/TGoogleFusionTableInputSource.java
+++ components/components-google/components-google-runtime/src/main/java/org/talend/components/google/runtime/reader/TGoogleFusionTableInputSource.java
@@ -12,6 +12,58 @@
 // ============================================================================
 package org.talend.components.google.runtime.reader;
 
-public class TGoogleFusionTableInputSource {
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.avro.Schema;
+import org.talend.components.api.component.runtime.BoundedReader;
+import org.talend.components.api.component.runtime.BoundedSource;
+import org.talend.components.api.container.RuntimeContainer;
+import org.talend.components.api.properties.ComponentProperties;
+import org.talend.daikon.NamedThing;
+import org.talend.daikon.properties.ValidationResult;
+
+public class TGoogleFusionTableInputSource implements BoundedSource {
+
+    @Override
+    public ValidationResult initialize(RuntimeContainer container, ComponentProperties properties) {
+        return null;
+    }
     
+    @Override
+    public ValidationResult validate(RuntimeContainer container) {
+        return null;
+    }
+    
+    @Override
+    public BoundedReader createReader(RuntimeContainer adaptor) {
+        return null;
+    }
+    
+    @Override
+    public List<NamedThing> getSchemaNames(RuntimeContainer container) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Schema getEndpointSchema(RuntimeContainer container, String schemaName) throws IOException {
+        return null;
+    }
+
+    @Override
+    public List<? extends BoundedSource> splitIntoBundles(long desiredBundleSizeBytes, RuntimeContainer adaptor)
+            throws Exception {
+        return null;
+    }
+
+    @Override
+    public long getEstimatedSizeBytes(RuntimeContainer adaptor) {
+        return 0;
+    }
+
+    @Override
+    public boolean producesSortedKeys(RuntimeContainer adaptor) {
+        return false;
+    }
+
 }
