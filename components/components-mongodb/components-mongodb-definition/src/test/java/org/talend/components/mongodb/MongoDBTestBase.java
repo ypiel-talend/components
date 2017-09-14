9d9906f74c5aacf3835e0e5880d1e2089e2942ac
jzhao <jzhao@talend.com>
2017-09-14 16:39:19 +0800

    feat(TDI-39250): Port MongoDB components to on TCOMP
    * fix issue when patch plugins into studio

--- components/components-mongodb/components-mongodb-definition/src/test/java/org/talend/components/mongodb/MongoDBTestBase.java
+++ components/components-mongodb/components-mongodb-definition/src/test/java/org/talend/components/mongodb/MongoDBTestBase.java
@@ -14,7 +14,6 @@ package org.talend.components.mongodb;
 
 import javax.inject.Inject;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
