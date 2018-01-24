aaa516f06a1b5f2a2723220a00431947f3415433
undx <emmanuel.gallois@gmail.com>
2018-01-24 11:21:27 +0000

    feat(TDI-39741): fix auto-service dependency

--- components/components-batchmode/src/main/java/org/talend/components/batchmode/BatchModeFamilyDefinition.java
+++ components/components-batchmode/src/main/java/org/talend/components/batchmode/BatchModeFamilyDefinition.java
@@ -1,10 +1,12 @@
 package org.talend.components.batchmode;
 
+import aQute.bnd.annotation.component.Component;
+
 import org.talend.components.api.AbstractComponentFamilyDefinition;
 import org.talend.components.api.ComponentInstaller;
 import org.talend.components.api.Constants;
 
-import aQute.bnd.annotation.component.Component;
+import com.google.auto.service.AutoService;
 
 @AutoService(ComponentInstaller.class)
 @Component(name = Constants.COMPONENT_INSTALLER_PREFIX + BatchModeFamilyDefinition.NAME, provide = ComponentInstaller.class)
