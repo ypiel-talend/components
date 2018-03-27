a276cc3e7557d56cab28174f9d1659726d4faa14
mbasiuk-talend <mbasiuk@talend.com>
2018-03-27 11:00:45 +0300

    Mbasiuk/tdi 40257 add safety switch property (#1221)
    * fix(TDI-40257): add new property safety switch with migration task.
    
    * fix(TDI-40257): add junits and update copyright.
    
    * fix(TDI-40257): add unit test for checking migration, remove migration
    code.
    
    # Conflicts:
    #	components/components-salesforce/components-salesforce-definition/src/main/java/org/talend/components/salesforce/tsalesforceinput/TSalesforceInputProperties.java
    #	components/components-salesforce/components-salesforce-definition/src/main/resources/org/talend/components/salesforce/tsalesforceinput/messages.properties
    #	components/components-salesforce/components-salesforce-definition/src/test/java/org/talend/components/salesforce/tsalesforceinput/TSalesforceInputPropertiesTest.java
    #	components/components-salesforce/components-salesforce-runtime/src/main/java/org/talend/components/salesforce/runtime/SalesforceBulkQueryInputReader.java
    #	components/components-salesforce/components-salesforce-runtime/src/main/java/org/talend/components/salesforce/runtime/SalesforceBulkRuntime.java

--- components/components-salesforce/components-salesforce-definition/src/test/java/org/talend/components/salesforce/tsalesforceinput/TSalesforceInputPropertiesTest.java
+++ components/components-salesforce/components-salesforce-definition/src/test/java/org/talend/components/salesforce/tsalesforceinput/TSalesforceInputPropertiesTest.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -88,6 +88,7 @@ public class TSalesforceInputPropertiesTest extends SalesforceTestBase {
         // check the result of setup layout
         mainForm = properties.getForm(Form.MAIN);
         advancedForm = properties.getForm(Form.ADVANCED);
+        Assert.assertNotNull(advancedForm.getWidget(properties.safetySwitch));
         Assert.assertNotNull(mainForm);
         Assert.assertNotNull(advancedForm);
     }
@@ -111,6 +112,7 @@ public class TSalesforceInputPropertiesTest extends SalesforceTestBase {
         Assert.assertFalse(properties.getForm(Form.ADVANCED).getWidget(properties.columnNameDelimiter.getName()).isHidden());
         Assert.assertFalse(properties.getForm(Form.ADVANCED).getWidget(properties.batchSize.getName()).isHidden());
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.jobTimeOut.getName()).isHidden());
+        Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.safetySwitch).isHidden());
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.pkChunking.getName()).isHidden());
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.chunkSize.getName()).isHidden());
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getChildForm(properties.connection.getName())
@@ -131,6 +133,7 @@ public class TSalesforceInputPropertiesTest extends SalesforceTestBase {
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.columnNameDelimiter.getName()).isHidden());
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.batchSize.getName()).isHidden());
         Assert.assertFalse(properties.getForm(Form.ADVANCED).getWidget(properties.jobTimeOut.getName()).isHidden());
+        Assert.assertTrue(properties.getForm(Form.ADVANCED).getWidget(properties.safetySwitch).isVisible());
         Assert.assertFalse(properties.getForm(Form.ADVANCED).getWidget(properties.pkChunking.getName()).isHidden());
         Assert.assertFalse(properties.getForm(Form.ADVANCED).getWidget(properties.chunkSize.getName()).isHidden());
         Assert.assertTrue(properties.getForm(Form.ADVANCED).getChildForm(properties.connection.getName())
