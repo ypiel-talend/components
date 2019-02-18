784b44bbe56a74c4b47ea2d5a9e5212a108cd5f2
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-02-18 14:28:17 +0100

    ensure to use a beam compatible google cloud stack

--- components/components-fileio/simplefileio-runtime/src/test/java/org/talend/components/simplefileio/runtime/gs/GSRoundTripRuntimeTestIT.java
+++ components/components-fileio/simplefileio-runtime/src/test/java/org/talend/components/simplefileio/runtime/gs/GSRoundTripRuntimeTestIT.java
@@ -64,7 +64,6 @@ public class GSRoundTripRuntimeTestIT {
 
     @Before
     public void prepare() {
-
         // DataflowPipelineOptions dataflowPO = pipelineOptions.as(DataflowPipelineOptions.class);
         // dataflowPO.setRunner(DataflowRunner.class);
 
