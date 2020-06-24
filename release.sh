de126b0db3a7b71881c59975d2c881095058cf9f
Ivan Gonchar <igonchar@talend.com>
2020-06-24 14:15:49 +0300

    fix(TDI-44235): fix typo in release word

--- release.sh
+++ release.sh
@@ -12,7 +12,7 @@ help(){
   echo "* This script uses git, so it should be installed"
   echo "* Make release.sh file executable. Run \"chmod +x release.sh\" to do it"
   echo "* Launch this script from root directory of components repository"
-  echo "* Checkout commit from which relese should be done before launching this script"
+  echo "* Checkout commit from which release should be done before launching this script"
   echo
   echo "Options:"
   echo "-h, --help          Display this help and exit"
