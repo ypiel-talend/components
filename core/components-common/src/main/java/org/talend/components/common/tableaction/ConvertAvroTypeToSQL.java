ac8eeb1e16bf658c7861dd6036433bd1782d27b8
ypiel <ypiel@talend.com>
2020-06-25 12:20:09 +0200

    Add a comment

--- core/components-common/src/main/java/org/talend/components/common/tableaction/ConvertAvroTypeToSQL.java
+++ core/components-common/src/main/java/org/talend/components/common/tableaction/ConvertAvroTypeToSQL.java
@@ -26,6 +26,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+// A new comment
 public class ConvertAvroTypeToSQL {
 
     private final static Map<Integer, String> SQLTypesMap = new HashMap<>();
